dea63f157f002023a21329c3f1be1a04b519e5b7
Mama-la-source <mamathagaje835@gmail.com>
2025-09-02 11:24:16 +0530

    first commit

--- src/component/Admin/Admin.jsx
+++ src/component/Admin/Admin.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import styles from './Admin.module.css'
+import  Skeleton from '@mui/material/Skeleton';
 
 const Admin = () => {
   return (
     <div className={styles.Admin}>
       <div className={styles.AdminBlock}>
+         <Skeleton
+              variant="rectangular"
+              width={266}
+              height={200}
+              sx={{ borderRadius: "20px" }}
+            />
         <div className={styles.AdminCard}>
           <h2> Coding Hunger</h2>
           <p style ={{ color:"blue"}}>mamathagaje835@gmail.com </p>
+          <h3> score : 50% </h3>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores natus facilis delectus molestias. Distinctio, eos minima deserunt suscipit quidem sit voluptatibus corporis. Necessitatibus, repellat labore? Quaerat, aut. Eveniet, consequatur voluptatibus.</p>
+      </div>
+       <div className={styles.AdminCard}>
+          <h2> Coding Hunger</h2>
+          <p style ={{ color:"blue"}}>mamathagaje835@gmail.com </p>
+          <h3> score : 50% </h3>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores natus facilis delectus molestias. Distinctio, eos minima deserunt suscipit quidem sit voluptatibus corporis. Necessitatibus, repellat labore? Quaerat, aut. Eveniet, consequatur voluptatibus.</p>
+      </div>
+         <div className={styles.AdminCard}>
+          <h2> Coding Hunger</h2>
+          <p style ={{ color:"blue"}}>mamathagaje835@gmail.com </p>
+          <h3> score : 50% </h3>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores natus facilis delectus molestias. Distinctio, eos minima deserunt suscipit quidem sit voluptatibus corporis. Necessitatibus, repellat labore? Quaerat, aut. Eveniet, consequatur voluptatibus.</p>
+      </div>
+           <div className={styles.AdminCard}>
+          <h2> Coding Hunger</h2>
+          <p style ={{ color:"blue"}}>mamathagaje835@gmail.com </p>
+          <h3> score : 50% </h3>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores natus facilis delectus molestias. Distinctio, eos minima deserunt suscipit quidem sit voluptatibus corporis. Necessitatibus, repellat labore? Quaerat, aut. Eveniet, consequatur voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur provident molestiae maxime magni error iusto qui facilis? Sint iure at cupiditate perferendis qui incidunt, iusto reprehenderit nesciunt, repudiandae libero nostrum!</p>
+      </div>
+           <div className={styles.AdminCard}>
+          <h2> Coding Hunger</h2>
+          <p style ={{ color:"blue"}}>mamathagaje835@gmail.com </p>
+          <h3> score : 50% </h3>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores natus facilis delectus molestias. Distinctio, eos minima deserunt suscipit quidem sit voluptatibus corporis. Necessitatibus, repellat labore? Quaerat, aut. Eveniet, consequatur voluptatibus.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi ad, dolore in dolorum sunt debitis perspiciatis cupiditate suscipit quia? Officia consequuntur dolorum consectetur, inventore sequi ipsum animi perferendis voluptatum? Quis!
+          </p>
       </div>
       </div>
     </div>
--- src/component/History/History.jsx
+++ src/component/History/History.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from './History.module.css'
+import  Skeleton from '@mui/material/Skeleton';
+
 
 const History = () => {
   return (
      <div className={styles.History}>
+      
       <div className={styles.HistoryCardBlock}>
+         <Skeleton
+              variant="rectangular"
+              width={266}
+              height={200}
+              sx={{ borderRadius: "20px" }}
+            />
         <div className={styles.HistoryCard}>
           <div className={styles.cardpercentage}>80%</div>
           <h2> Frontend Developer </h2>
